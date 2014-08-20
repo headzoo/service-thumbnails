0b3ab2d8f6e9f28ae459ffb776d4ca3ecb0ee4ae
headzoo <sean@headzoo.io>
2014-08-20 15:59:22 -0400

    Initial commit

---
+++ ffmpeg/ffmpeg.go
@@ -0,0 +1,152 @@
+package ffmpeg
+
+import (
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"fmt"
+)
+
+// FFmpeg is used to create thumbnails from videos.
+type FFmpeg struct {
+	SkipSeconds int
+	Video string
+	TmpDirectory string
+
+	cmdFFprobe string
+	cmdFFmpeg string
+	cmdConvert string
+}
+
+// Creates and returns a new FFmpeg instance.
+func NewFFmpeg(video string) *FFmpeg {
+	//ffmpeg_path := os.Getenv("HW_FFMPEG_PATH")
+	//if ffmpeg_path == "" {
+	//	panic("Environment variables HW_FFMPEG_PATH not set.")
+	//}
+
+	return &FFmpeg{
+		SkipSeconds: 0,
+		Video: video,
+		TmpDirectory: "/tmp",
+
+		cmdFFprobe: "/home/sean/lib/FFmpeg/ffprobe",
+		cmdFFmpeg: "/home/sean/lib/FFmpeg/ffmpeg",
+		cmdConvert: "/usr/bin/convert",
+	}
+}
+
+// Length returns the length of the video in seconds.
+func (f *FFmpeg) Length() float64 {
+	output, err := exec.Command(
+		f.cmdFFprobe,
+		"-i",
+		f.Video,
+		"-v",
+		"quiet",
+		"-show_entries",
+		"format=duration",
+		"-of",
+		"csv=p=0",
+	).Output()
+	if err != nil {
+		return 0.0
+	}
+
+	len, err := strconv.ParseFloat(strings.Trim(string(output), "\n"), 64)
+	if err != nil {
+		return 0.0
+	}
+
+	return len
+}
+
+// CreateThumbnail creates a single thumbnail from the video.
+// The frame is determined by the value of FFmpeg.SkipSeconds.
+// When 0 is given for the 'width' argument, the thumbnail will have the same
+// width of the video.
+func (f *FFmpeg) CreateThumbnail(width int, outFile string) error {
+	os.Remove(outFile)
+
+	args := []string{
+		"-ss",
+		SecondsToTime(f.SkipSeconds),
+		"-i",
+		f.Video,
+		"-f",
+		"image2",
+		"-vframes",
+		"1",
+	}
+	if width != 0 {
+		args = append(args, "-vf")
+		args = append(args, fmt.Sprintf("scale='min(%d\\,iw)':-1", width))
+	}
+	args = append(args, outFile)
+
+	err := exec.Command(f.cmdFFmpeg, args...).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CreateThumbnailSprite creates thumbnails from the video at the given interval,
+// and stitches them together into a single sprite.
+// A thumbnail is generated every 'interval' seconds with a max width of 'width'.
+// The thumbnails are then stitched together into a single image written to 'outFile'.
+func (f *FFmpeg) CreateThumbnailSprite(interval, width int, outFile string) error {
+	os.Remove(outFile)
+	err := os.MkdirAll(f.TmpDirectory, os.FileMode(0755))
+	if err != nil {
+		return err
+	}
+
+	filters := []string{
+		fmt.Sprintf("fps=fps=1/%d", interval),
+		fmt.Sprintf("scale='min(%d\\,iw)':-1", width),
+	}
+
+	err = exec.Command(
+		f.cmdFFmpeg,
+		"-i",
+		f.Video,
+		"-ss",
+		SecondsToTime(f.SkipSeconds),
+		"-f",
+		"image2",
+		"-vf",
+		strings.Join(filters, ","),
+		f.TmpDirectory + "/frames%04d.jpg",
+	).Run()
+	if err != nil {
+		return err
+	}
+
+	err = exec.Command(
+		f.cmdConvert,
+		f.TmpDirectory + "/*.jpg",
+		"+append",
+		outFile,
+	).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// SecondsToTime converts seconds into "00:00:00" format.
+func SecondsToTime(secs int) string {
+	if secs == 0 {
+		return "00:00:00"
+	}
+
+	hours  := secs / 3600;
+	minutes := (secs - (hours * 3600)) / 60;
+	seconds := secs - (hours * 3600) -  (minutes * 60);
+
+	return fmt.Sprintf("%.2d:%.2d:%.2d", hours, minutes, seconds)
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+
+	 "github.com/headzoo/thumbnailer/ffmpeg"
+	"fmt"
+)
+
+const NUM_THUMBNAILS = 30
+
+func main() {
+	srcFileName := "/home/sean/Downloads/big_buck_bunny.mp4"
+
+	f := ffmpeg.NewFFmpeg(srcFileName)
+	f.SkipSeconds = 10
+
+	len := int(f.Length())
+	interval := len / NUM_THUMBNAILS
+	fmt.Printf("len = %d, thumbs = %d\n", len, interval)
+	err := f.CreateThumbnailSprite(interval, 180, "./thumbnail.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	f.SkipSeconds = 0
+	err = f.CreateThumbnail(0, "./thumbnail-big.jpg")
+	if err != nil {
+		panic(err)
+	}
+}
