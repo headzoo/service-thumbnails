36af1ba882ee76e9bed1d840af7bf54ec35f7875
headzoo <sean@headzoo.io>
2014-08-20 17:44:26 -0400

    Concurrency fix

--- main.go
+++ main.go
@@ -5,52 +5,55 @@ import (
 	"fmt"
 	"github.com/dulo-tech/thumbnailer/ffmpeg"
 	"os"
+	"strings"
 )
 
 // Misc constants.
 const (
-	VERSION = "0.1"
+	VERSION        = "0.1"
 	NUM_THUMBNAILS = 30
 )
 
 // Default values for command line options.
 const (
-	OPT_THUMB_TYPE = ""
-	OPT_IN_FILE = ""
-	OPT_OUT_FILE = ""
-	OPT_WIDTH = 0
+	OPT_THUMB_TYPE   = ""
+	OPT_IN_FILE      = ""
+	OPT_OUT_FILE     = ""
+	OPT_WIDTH        = 0
 	OPT_SKIP_SECONDS = 0
-	OPT_TEMP_DIR = "/tmp"
-	OPT_VERBOSE = false
-	OPT_PRINT_HELP = false
+	OPT_TEMP_DIR     = "/tmp"
+	OPT_VERBOSE      = false
+	OPT_PRINT_HELP   = false
 )
 
-// Signals for the app to exit.
-type ChannelExit chan bool
+// Signals a job is finished.
+type ChannelFinished chan bool
 
 // Signals an error.
 type ChannelError chan error
 
 // Options stores the command line options.
 type Options struct {
-	ThumbType string
-	InFile    string
-	OutFile   string
-	Width     int
-	SkipSeconds int
+	ThumbType     string
+	InFile        string
+	OutFile       string
+	Width         int
+	SkipSeconds   int
 	TempDirectory string
-	Verbose   bool
-	PrintHelp bool
+	Verbose       bool
+	PrintHelp     bool
 }
 
 var opts = Options{}
+var chanFinished = make(ChannelFinished)
+var chanError = make(ChannelError)
 
 func main() {
 	flag.BoolVar(&opts.PrintHelp, "h", OPT_PRINT_HELP, "Display command help.")
 	flag.BoolVar(&opts.Verbose, "v", OPT_VERBOSE, "Verbose output.")
 	flag.StringVar(&opts.ThumbType, "t", OPT_THUMB_TYPE, "The type of thumbnail to generate.")
-	flag.StringVar(&opts.InFile, "i", OPT_IN_FILE, "The input video file.")
-	flag.StringVar(&opts.OutFile, "o", OPT_OUT_FILE, "The output image file.")
+	flag.StringVar(&opts.InFile, "i", OPT_IN_FILE, "The input video file. Separate multiple files with a comma.")
+	flag.StringVar(&opts.OutFile, "o", OPT_OUT_FILE, "The output image file. Separate multiple files with a comma.")
 	flag.IntVar(&opts.Width, "w", OPT_WIDTH, "The thumbnail width. Overrides the built in defaults.")
 	flag.IntVar(&opts.SkipSeconds, "s", OPT_SKIP_SECONDS, "Skip this number of seconds into the video before thubmnailing.")
 	flag.StringVar(&opts.TempDirectory, "d", OPT_TEMP_DIR, "Temp directory.")
@@ -62,56 +65,82 @@ func main() {
 	if opts.ThumbType != "sprite" && opts.ThumbType != "big" {
 		printHelp()
 	}
-	if _, err := os.Stat(opts.InFile); os.IsNotExist(err) {
-		fmt.Println("The input video file does not exist.")
+
+	inFiles := strings.Split(opts.InFile, ",")
+	outFiles := strings.Split(opts.OutFile, ",")
+	for i, f := range inFiles {
+		inFiles[i] = strings.Trim(f, " ")
+	}
+	for i, f := range outFiles {
+		outFiles[i] = strings.Trim(f, " ")
+	}
+	if len(inFiles) != len(outFiles) {
+		verboseError("The number of in files must match the number of out files.")
 		os.Exit(1)
 	}
+	for _, file := range inFiles {
+		if !fileExists(file) {
+			verboseError(fmt.Sprintf("The input file %q does not exist.", file))
+			os.Exit(1)
+		}
+	}
 
-	chanExit := make(ChannelExit)
-	chanError := make(ChannelError)
-
+	verbose(fmt.Sprintf("Thumbnailing %d video(s).", len(inFiles)))
 	if opts.ThumbType == "big" {
-		go createBigThumbnail(opts.InFile, chanExit, chanError)
+		for i, file := range inFiles {
+			go createBigThumbnail(file, outFiles[i])
+		}
 	} else if opts.ThumbType == "sprite" {
-		go createSpriteThumbnail(opts.InFile, chanExit, chanError)
+		for i, file := range inFiles {
+			go createSpriteThumbnail(file, outFiles[i])
+		}
 	} else {
 		printHelp()
 	}
 
+	finished := 0
+	numJobs := len(inFiles)
 	for {
 		select {
 		case err := <-chanError:
-			verbose(err.Error())
-			os.Exit(1)
-		case <-chanExit:
-			verbose(fmt.Sprintf("Thumbnail written to file %q.", opts.OutFile))
-			os.Exit(0)
+			{
+				verbose(err.Error())
+				os.Exit(1)
+			}
+		case <-chanFinished:
+			{
+				finished++
+				if finished == numJobs {
+					verbose("Finished")
+					os.Exit(0)
+				}
+			}
 		}
 	}
 }
 
 // createBigThumbnail creates a big thumbnail.
-func createBigThumbnail(inFile string, chanExit ChannelExit, chanError ChannelError) {
-	verbose("Creating 'big' thumbnail...")
+func createBigThumbnail(inFile, outFile string) {
 	defer func() {
-		close(chanExit)
+		chanFinished <- true
 	}()
 
 	f := ffmpeg.NewFFmpeg(inFile)
 	f.TmpDirectory = opts.TempDirectory
 	f.SkipSeconds = opts.SkipSeconds
 
-	err := f.CreateThumbnail(opts.Width, opts.OutFile)
+	err := f.CreateThumbnail(opts.Width, outFile)
 	if err != nil {
 		chanError <- err
+		return
 	}
+	verbose(fmt.Sprintf("Thumbnail written to file %q.", outFile))
 }
 
 // createSpriteThumbnail creates a sprite thumbnail.
-func createSpriteThumbnail(inFile string, chanExit ChannelExit, chanError ChannelError) {
-	verbose("Creating 'strip' thumbnail...")
+func createSpriteThumbnail(inFile, outFile string) {
 	defer func() {
-		close(chanExit)
+		chanFinished <- true
 	}()
 
 	f := ffmpeg.NewFFmpeg(inFile)
@@ -119,16 +148,24 @@ func createSpriteThumbnail(inFile string, chanExit ChannelExit, chanError Channe
 	f.SkipSeconds = opts.SkipSeconds
 
 	len := int(f.Length())
-	interval := len / NUM_THUMBNAILS
+	interval := 0
+	if len < NUM_THUMBNAILS {
+		interval = len
+	} else {
+		interval = len / NUM_THUMBNAILS
+	}
+
 	width := 180
 	if opts.Width != 0 {
 		width = opts.Width
 	}
 
-	err := f.CreateThumbnailSprite(interval, width, opts.OutFile)
+	err := f.CreateThumbnailSprite(interval, width, outFile)
 	if err != nil {
 		chanError <- err
+		return
 	}
+	verbose(fmt.Sprintf("Thumbnail written to file %q.", outFile))
 }
 
 // printHelp() prints the command line help and exits.
@@ -145,6 +182,7 @@ func printHelp() {
 	fmt.Println("")
 	fmt.Println("EXAMPLE:")
 	fmt.Println("thumbnailer -v -t strip -i source.mp4 -o thumb.jpg")
+	fmt.Println("thumbnailer -v -t big -i source1.mp4,source2.mp4 -o out1.jpg,out2.jpg")
 
 	os.Exit(1)
 }
@@ -154,4 +192,17 @@ func verbose(msg string) {
 	if opts.Verbose {
 		fmt.Println(msg)
 	}
+}
+
+// verbose prints the given message when verbose output is turned on.
+func verboseError(msg string) {
+	if opts.Verbose {
+		fmt.Fprintln(os.Stderr, msg)
+	}
+}
+
+// fileExists returns whether the given file exists.
+func fileExists(file string) bool {
+	_, err := os.Stat(file)
+	return !os.IsNotExist(err)
 }
