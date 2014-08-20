02f34704f5797ee89211617258e9eecb23283e0c
headzoo <sean@headzoo.io>
2014-08-20 17:13:51 -0400

    Added concurrency

--- main.go
+++ main.go
@@ -19,9 +19,17 @@ const (
 	OPT_IN_FILE = ""
 	OPT_OUT_FILE = ""
 	OPT_WIDTH = 0
+	OPT_SKIP_SECONDS = 0
+	OPT_TEMP_DIR = "/tmp"
 	OPT_VERBOSE = false
 	OPT_PRINT_HELP = false
 )
+
+// Signals for the app to exit.
+type ChannelExit chan bool
+
+// Signals an error.
+type ChannelError chan error
 
 // Options stores the command line options.
 type Options struct {
@@ -29,6 +37,8 @@ type Options struct {
 	InFile    string
 	OutFile   string
 	Width     int
+	SkipSeconds int
+	TempDirectory string
 	Verbose   bool
 	PrintHelp bool
 }
@@ -42,12 +52,14 @@ func main() {
 	flag.StringVar(&opts.InFile, "i", OPT_IN_FILE, "The input video file.")
 	flag.StringVar(&opts.OutFile, "o", OPT_OUT_FILE, "The output image file.")
 	flag.IntVar(&opts.Width, "w", OPT_WIDTH, "The thumbnail width. Overrides the built in defaults.")
+	flag.IntVar(&opts.SkipSeconds, "s", OPT_SKIP_SECONDS, "Skip this number of seconds into the video before thubmnailing.")
+	flag.StringVar(&opts.TempDirectory, "d", OPT_TEMP_DIR, "Temp directory.")
 	flag.Parse()
 
 	if opts.PrintHelp || opts.InFile == "" || opts.OutFile == "" || opts.ThumbType == "" {
 		printHelp()
 	}
-	if opts.ThumbType != "strip" && opts.ThumbType != "big" {
+	if opts.ThumbType != "sprite" && opts.ThumbType != "big" {
 		printHelp()
 	}
 	if _, err := os.Stat(opts.InFile); os.IsNotExist(err) {
@@ -55,36 +67,67 @@ func main() {
 		os.Exit(1)
 	}
 
-	f := ffmpeg.NewFFmpeg(opts.InFile)
-	f.TmpDirectory = "/tmp"
+	chanExit := make(ChannelExit)
+	chanError := make(ChannelError)
 
 	if opts.ThumbType == "big" {
-		verbose("Creating 'big' thumbnail...")
-		f.SkipSeconds = 0
-		width := opts.Width
-		err := f.CreateThumbnail(width, opts.OutFile)
-		if err != nil {
-			panic(err)
-		}
-		verbose("Finished!")
-	} else if opts.ThumbType == "strip" {
-		verbose("Creating 'strip' thumbnail...")
-		f.SkipSeconds = 10
-
-		len := int(f.Length())
-		interval := len / NUM_THUMBNAILS
-		width := 180
-		if opts.Width != 0 {
-			width = opts.Width
-		}
-
-		err := f.CreateThumbnailSprite(interval, width, opts.OutFile)
-		if err != nil {
-			panic(err)
-		}
-		verbose("Finished!")
+		go createBigThumbnail(opts.InFile, chanExit, chanError)
+	} else if opts.ThumbType == "sprite" {
+		go createSpriteThumbnail(opts.InFile, chanExit, chanError)
 	} else {
 		printHelp()
+	}
+
+	for {
+		select {
+		case err := <-chanError:
+			verbose(err.Error())
+			os.Exit(1)
+		case <-chanExit:
+			verbose(fmt.Sprintf("Thumbnail written to file %q.", opts.OutFile))
+			os.Exit(0)
+		}
+	}
+}
+
+// createBigThumbnail creates a big thumbnail.
+func createBigThumbnail(inFile string, chanExit ChannelExit, chanError ChannelError) {
+	verbose("Creating 'big' thumbnail...")
+	defer func() {
+		close(chanExit)
+	}()
+
+	f := ffmpeg.NewFFmpeg(inFile)
+	f.TmpDirectory = opts.TempDirectory
+	f.SkipSeconds = opts.SkipSeconds
+
+	err := f.CreateThumbnail(opts.Width, opts.OutFile)
+	if err != nil {
+		chanError <- err
+	}
+}
+
+// createSpriteThumbnail creates a sprite thumbnail.
+func createSpriteThumbnail(inFile string, chanExit ChannelExit, chanError ChannelError) {
+	verbose("Creating 'strip' thumbnail...")
+	defer func() {
+		close(chanExit)
+	}()
+
+	f := ffmpeg.NewFFmpeg(inFile)
+	f.TmpDirectory = opts.TempDirectory
+	f.SkipSeconds = opts.SkipSeconds
+
+	len := int(f.Length())
+	interval := len / NUM_THUMBNAILS
+	width := 180
+	if opts.Width != 0 {
+		width = opts.Width
+	}
+
+	err := f.CreateThumbnailSprite(interval, width, opts.OutFile)
+	if err != nil {
+		chanError <- err
 	}
 }
 
@@ -93,7 +136,7 @@ func printHelp() {
 	fmt.Printf("Thumbnailer v%s\n", VERSION)
 	fmt.Println("")
 	fmt.Println("USAGE:")
-	fmt.Println("thumbnailer -t <strip|big> -i <video> -o <image>")
+	fmt.Println("thumbnailer -t <sprite|big> -i <video> -o <image>")
 	fmt.Println("")
 	fmt.Println("OPTIONS:")
 	flag.VisitAll(func(f *flag.Flag) {
