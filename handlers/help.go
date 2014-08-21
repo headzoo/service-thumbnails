522b848625212f4e70556ba251a04984b81b33e5
headzoo <sean@headzoo.io>
2014-08-21 19:54:06 -0400

    Removed html template

--- handlers/help.go
+++ handlers/help.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"html/template"
 	"net/http"
-	"os"
 
 	"github.com/dulo-tech/thumbnailer/thumbnailer"
 )
@@ -33,8 +32,7 @@ func (h *HelpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		DefaultSkip:  h.opts.SkipSeconds,
 	}
 
-	dir, _ := os.Getwd()
-	t, err := template.ParseFiles(dir + "/handlers/templates/help.html")
+	t, err := template.New("help").Parse(helpTemplate)
 	if err != nil {
 		numErrors++
 		w.WriteHeader(500)
@@ -45,3 +43,53 @@ func (h *HelpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	numRequests++
 	t.Execute(w, data)
 }
+
+const helpTemplate = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Help</title>
+    </head>
+    <body>
+        <h1>Thumbnailer Help</h1>
+        <p>End Points:</p>
+        <ul>
+            <li>
+                POST <a href="/thumbnail/simple">/thumbnail/simple</a>
+                <p>
+                    Generates a simple thumbnail from an uploaded video. A single video must be uploaded.
+                    <br/>Possible query arguments:
+                    <ul>
+                        <li>width - The width of the thumbnail. Defaults to the width of the video.</li>
+                        <li>skip - Skip this number of seconds into the video. Defaults to {{.DefaultSkip}}.</li>
+                    </ul>
+                </p>
+            </li>
+            <li>
+                POST <a href="/thumbnail/sprite">/thumbnail/sprite</a>
+                <p>
+                    Generates a sprite thumbnail from an uploaded video. A single video must be uploaded.
+                    <br/>Possible query arguments:
+                    <ul>
+                        <li>width - The width of the thumbnail. Defaults to 180px wide maintaining aspect ratio.</li>
+                        <li>skip - Skip this number of seconds into the video. Defaults to {{.DefaultSkip}}.</li>
+                        <li>count - The number of thumbnails to include in the sprite. Defaults to {{.DefaultCount}}.</li>
+                    </ul>
+                </p>
+            </li>
+            <li>
+                GET <a href="/help">/help</a>
+                <p>
+                    Returns this help page.
+                </p>
+            </li>
+            <li>
+                GET <a href="/pulse">/pulse</a>
+                <p>
+                    Returns health information for the server. See <a href="https://github.com/dulo-tech/amsterdam/wiki/Specification:-Pulse-Protocol">Pulse Protocol</a>.
+                </p>
+            </li>
+        </ul>
+    </body>
+</html>
+`
